e8504bdf3cd04c39156b65717c619dbcb2d0f9a5
najorcruzcruz <najorcruzcruz@gmail.com>
2016-10-06 03:14:21 +0100

    Scoring project koan completed.

--- about_scoring_project.rb
+++ about_scoring_project.rb
@@ -30,7 +30,37 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 # Your goal is to write the score method.
 
 def score(dice)
-  # You need to write this method
+  number_repeated = []
+
+  1.upto(6) do |index|
+    number_repeated[index] = 0
+  end
+
+  dice.each do |item|
+    number_repeated[item] += 1
+  end
+
+  points = 0
+  1.upto(6) do |index|
+    if index == 1
+      if number_repeated[index] >= 3
+        points += 1000
+        number_repeated[index] -= 3
+      end
+      points += number_repeated[index] * 100
+    end
+
+    if number_repeated[index] >= 3
+      points += index * 100
+      number_repeated[index] -= 3
+    end
+
+    if index == 5
+      points += number_repeated[index] * 50
+    end
+  end
+
+  points
 end
 
 class AboutScoringProject < Neo::Koan
