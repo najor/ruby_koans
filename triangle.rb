b7aaf7b5842b72b025808416290660dc5ef7abf4
najorcruzcruz <najorcruzcruz@gmail.com>
2017-03-30 12:56:00 +0100

    Triangle Koans complete

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,13 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+
+  raise TriangleError, 'Sides cannot be less or equal to 0' if a <= 0 || b <= 0 || c <= 0
+  raise TriangleError, 'Sides length' if [a, b , c].max >= (a + b + c) / 2.0
+
+  return :equilateral if a == b && b == c
+  return :isosceles if a == b || b == c || a == c
+  :scalene
 end
 
 # Error class used in part 2.  No need to change this code.
